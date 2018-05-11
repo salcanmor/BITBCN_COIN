2a796342bf6dfdf4fc9eac4b4acf7f57462b6f02
SALVADOR CANAS MORENO <cybero@hotmail.es>
2018-05-11 22:29:44 +0200

    Código fuente de la moneda BITCOIN_COIN

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /root/cryptonote
+# Build directory: /root/cryptonote
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(external)
+subdirs(src)
+subdirs(tests)
